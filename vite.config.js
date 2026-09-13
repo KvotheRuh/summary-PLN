fbd3f770d6a885c3c20f3e64df977c764304c747
Carlos Morciani <146388143+KvotheRuh@users.noreply.github.com>
2026-09-12 22:55:03 -0300

    Fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/summary-PLN--course/",
+  base: "/summary-PLN/",
   plugins: [react()],
 });
