5d651b4bfa91c49e72af03f7a4c31c961cd74883
ana <114084376+ana-borowsky@users.noreply.github.com>
2026-09-12 22:37:08 -0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/summary-AI--course/",
+  base: "/summary-PLN--course/",
   plugins: [react()],
 });
